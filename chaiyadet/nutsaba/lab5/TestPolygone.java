025062f33e979bc158e498f08d3abff2ec96f621
nutsaba <nutsaba@MacBook-Air-khxng-nutsaba.local>
2023-01-14 00:04:11 +0700

    lab4

---
+++ chaiyadet/nutsaba/lab5/TestPolygone.java
@@ -0,0 +1,48 @@
+package chaiyadet.nutsaba.lab5;
+
+enum PolygonType {RECTANGLE,TRIANGLE}
+
+class Polygon {
+    float width;
+    float height;
+    PolygonType type;
+    double area;
+    static int  numPolygons ;
+    static double sumPolygonsArea ;
+   public double getArea() {
+     if (type == PolygonType.RECTANGLE){
+        return width*height;
+     }
+     else {
+        return (width*height)/2;
+     }
+   }
+   public static void setNumPolygons(int numNewPolygons) {
+        numPolygons += numNewPolygons;
+   } 
+   public static void setSumPolygonsArea(double newPolygonsArea) {
+        sumPolygonsArea += newPolygonsArea;
+   }
+}
+
+public class TestPolygone {
+    public static void main(String[] args) {
+    Polygon polygon1 = new Polygon();
+    polygon1.width = 2;
+    polygon1.height = 3;
+    polygon1.type = PolygonType.RECTANGLE;
+    Polygon.setNumPolygons(1);
+    Polygon.setSumPolygonsArea(polygon1.getArea());
+    Polygon polygon2 = new Polygon();
+    polygon2.width = 3;
+    polygon2.height = 4;
+    polygon2.type = PolygonType.TRIANGLE;
+    Polygon.setNumPolygons(1);
+    Polygon.setSumPolygonsArea(polygon2.getArea());
+    System.out.println("The number of polygons is " + Polygon.numPolygons + " or " + polygon1.numPolygons + " or " + polygon2.numPolygons);
+    System.out.println("The width of polygon1 is " + polygon1.width + " the width of polygon2 is " + polygon2.width);
+    System.out.println("The sum of polygons area is " + Polygon.sumPolygonsArea);
+    }
+}
+
+
